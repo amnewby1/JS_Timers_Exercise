758719338514996469762782bd7772cf799e973c
amnewby1 <114841649+amnewby1@users.noreply.github.com>
2022-11-19 23:20:03 -0500

    Add files via upload

---
+++ JS_Timers_Exercise.js
@@ -0,0 +1,24 @@
+function countDown(time) {
+  let timer = setInterval(function () {
+    time--;
+    if (time <= 0) {
+      clearInterval(timer);
+      console.log("DONE!");
+    } else {
+      console.log(time);
+    }
+  }, 1000);
+}
+
+function randomGame() {
+  let num;
+  let times = 0;
+  let timer = setInterval(function () {
+    num = Math.random();
+    times++;
+    if (num > 0.75) {
+      clearInterval(timer);
+      console.log("It took " + times + " try/tries.");
+    }
+  }, 1000);
+}
